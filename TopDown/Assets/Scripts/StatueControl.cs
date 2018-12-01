798bb35f88289c4e8c9d5a9ff3888a642011fc6f
weirdstoies <daniltatarinof@mail.ru>
2018-12-02 00:04:51 +0300

    Make vision cone

---
+++ TopDown/Assets/Scripts/StatueControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueControl : MonoBehaviour
+{
+
+	public Collider2D visionCol;
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+//		while (GetComponent<Collider2D>().IsTouching(visionCol))
+//		{
+//			Debug.Log("Видно!");
+//		}
+	}
+	
+}
---
+++ TopDown/Assets/Scripts/VisionCast.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class VisionCast : MonoBehaviour
+{
+
+//	public float visionDistance;
+//
+//	private float currentDistance;
+//
+//	private void Update()
+//	{
+//		currentDistance = Physics2D.Raycast(transform.position, transform.position + transform.up, visionDistance).distance;
+//		Debug.DrawLine(transform.position, transform.up * 2);
+//	}
+}
