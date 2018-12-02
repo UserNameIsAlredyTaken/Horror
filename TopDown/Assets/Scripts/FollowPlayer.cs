07f7c1d9fa1b4658ff9aada6e6714b6e3ea0ce2e
weirdstoies <daniltatarinof@mail.ru>
2018-12-02 13:34:13 +0300

    Statues follows player

---
+++ TopDown/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public GameObject player;
+	public Collider2D vision;
+	public float speed;
+
+	void FixedUpdate () {
+		if (!Physics2D.IsTouching(GetComponent<Collider2D>(), vision))
+		{
+			transform.Translate((player.transform.position - transform.position) * speed * Time.deltaTime);
+		}
+	}
+}
