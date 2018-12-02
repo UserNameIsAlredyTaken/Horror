77e4fd8862f3bc4296caa658cb25177eaf0ff551
weirdstoies <daniltatarinof@mail.ru>
2018-12-03 00:28:05 +0300

    Players speed is effected by direction

--- TopDown/Assets/Scripts/PlayerMovementControl.cs
+++ TopDown/Assets/Scripts/PlayerMovementControl.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerMovementControl : MonoBehaviour
 {
-
-	public float forwardSpeed;
-	public float backwardSpeed;
+	//more this value, less difference between forward speed and backward speed (1 is the minimal value, with which backward speed equals 0)
+	public float speedCoef = 1;
+	public float speed;
 
 	private Rigidbody2D thisRigidbody;
 	private float forwardInput;
@@ -31,16 +31,12 @@ public class PlayerMovementControl : MonoBehaviour
 		Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
 		transform.rotation = rot;
 		transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z); //block rotating on Y and X axis
-//		GetComponent<Rigidbody2D>().angularVelocity = 0;
 	}
 
 	private void Move()
 	{
 		Vector2 movmentDirection = Vector2.up * forwardInput + Vector2.right * sidwardInput; //finding direction of movement
-		Vector2 movmentAmount = (forwardInput < 0) ? //using  different speeds whether we go forward or backward 
-			backwardSpeed * movmentDirection * Time.deltaTime :
-			forwardSpeed * movmentDirection * Time.deltaTime;
-		thisRigidbody.MovePosition(thisRigidbody.position + movmentAmount);//apply movment
-		
+		Vector2 movmentAmount = (Vector2.Dot(movmentDirection, transform.up) + speedCoef) * speed * movmentDirection * Time.deltaTime; //using  different speeds whether we go forward or backward //		
+		thisRigidbody.MovePosition(thisRigidbody.position + movmentAmount);//apply movement
 	}
 }
